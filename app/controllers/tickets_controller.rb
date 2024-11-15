ea88db96c6a10c92c0f391e434ce720e2c623081
Ulisses Piassa <ulisses.piassa@gmail.com>
2024-11-15 05:55:16 -0300

    Criado o endpoint de contagem de Tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -13,10 +13,18 @@ class TicketsController < ApplicationController
         end
     end
 
+    def tickets_count
+        start_date = params[:start_date].to_date
+        end_date = params[:end_date].to_date
+
+        count = Ticket.where(created_at: start_date.beginning_of_day..end_date.end_of_day).count
+
+        render json: { tickets_count: count }
+    end
     private
 
     def ticket_params
         params.require(:ticket).permit(:client_name)
     end
-    
+
 end
