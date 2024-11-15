2e34ccf04b8b21454d61e01e2a59a4f31e696717
Ulisses Piassa <ulisses.piassa@gmail.com>
2024-11-15 06:14:00 -0300

    Implementada a rota para exportar os tickets em formato CSV

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
     resources :messages, only: [:index, :create]
   end
 
+  # rota implementada para contagem dos tickets em um intervalo de datas:
   get 'api/v1/reports/tickets_count', to: 'tickets#tickets_count'
+
+  # rota implementada para exportação dos tickets como csv
+  get 'api/v1/reports/tickets.csv', to: 'tickets#tickets_csv'
+  
 end
