4ce950df6d74880c790ba204c697e499e52a02dd
Ulisses Piassa <ulisses.piassa@gmail.com>
2024-11-15 06:54:14 -0300

    Implementada a autenticação simples da API (hardcoded por problemas na WSL)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_api
+
+    private
+  
+    def authenticate_api
+      token = request.headers['Authorization']
+      if token != "Bearer wh1zzt3ch"
+        render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+    end
 end
